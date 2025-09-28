6b9ae08c2f0b2472093a967e2d8d5e4809d83a09
Nonpawit P <nonpawit.p@outlook.com>
2025-09-28 16:10:03 +0700

    fix: แก้ topup modal ให้ใช้ได้ทุกหน้าของฝั่งผู้ใช้งาน

--- frontend/src/app/layouts/member-layout/member-layout.ts
+++ frontend/src/app/layouts/member-layout/member-layout.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MemberSidebar } from '../../components/common/member-sidebar/member-sidebar';
 import { RouterOutlet } from '@angular/router';
+import { TopupModal } from '../../components/home/topup-modal/topup-modal';
+import { TopupService } from '../../services/topup.service';
+import { UserService } from '../../services/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-member-layout',
-  imports: [MemberSidebar, RouterOutlet],
+  imports: [MemberSidebar, RouterOutlet, TopupModal],
   templateUrl: './member-layout.html',
   styleUrl: './member-layout.css',
 })
-export class MemberLayout {}
+export class MemberLayout {
+  topupService = inject(TopupService);
+  userService = inject(UserService);
+
+  onTopupSubmit = (amount: number) => {
+    this.userService.userProfile.wallet += amount;
+    this.topupService.set(false);
+    Swal.fire({
+      title: 'สำเร็จ',
+      text: 'เติมเงินสำเร็จ',
+      icon: 'success',
+    });
+  };
+}
--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,31 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { TrendingGame } from '../../components/home/trending-game/trending-game';
 import { GameCard } from '../../components/home/game-card/game-card';
-import { TopupModal } from '../../components/home/topup-modal/topup-modal';
-import { TopupService } from '../../services/topup.service';
-import { UserService } from '../../services/user.service';
-import Swal from 'sweetalert2';
 import { GameService } from '../../services/game.service';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [TrendingGame, GameCard, TopupModal, AsyncPipe],
+  imports: [TrendingGame, GameCard, AsyncPipe],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-  topupService = inject(TopupService);
-  userService = inject(UserService);
   gameService = inject(GameService);
-
-  onTopupSubmit = (amount: number) => {
-    this.userService.userProfile.wallet += amount;
-    this.topupService.set(false);
-    Swal.fire({
-      title: 'สำเร็จ',
-      text: 'เติมเงินสำเร็จ',
-      icon: 'success',
-    });
-  };
 }
