be9eab2bbc4de2032d5b84a049619562ebc82ebf
Nonpawit P. <nonpawit.p@outlook.com>
2025-09-29 23:30:43 +0700

    feat: เพิ่มระบบเติมเงิน

--- frontend/src/app/layouts/member-layout/member-layout.ts
+++ frontend/src/app/layouts/member-layout/member-layout.ts
@@ -5,6 +5,8 @@ import { TopupModal } from '../../components/home/topup-modal/topup-modal';
 import { TopupService } from '../../services/topup.service';
 import { UserService } from '../../services/user.service';
 import Swal from 'sweetalert2';
+import { AuthService } from '../../services/auth.service';
+import { switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'app-member-layout',
@@ -15,14 +17,28 @@ import Swal from 'sweetalert2';
 export class MemberLayout {
   topupService = inject(TopupService);
   userService = inject(UserService);
+  authService = inject(AuthService);
 
   onTopupSubmit = (amount: number) => {
-    this.userService.userProfile.wallet += amount;
-    this.topupService.set(false);
-    Swal.fire({
-      title: 'สำเร็จ',
-      text: 'เติมเงินสำเร็จ',
-      icon: 'success',
-    });
+    this.authService.currentUser$
+      .pipe(
+        take(1),
+        switchMap((user) => this.userService.topup(user!.id, amount)),
+      )
+      .subscribe((response) => {
+        if (response.success) {
+          Swal.fire({
+            title: 'สำเร็จ',
+            text: response.message,
+            icon: 'success',
+          });
+        } else {
+          Swal.fire({
+            title: 'เกิดข้อผิดพลาด',
+            text: response.message,
+            icon: 'error',
+          });
+        }
+      });
   };
 }
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,31 +1,43 @@
-import { inject, Injectable, signal } from '@angular/core';
-import { doc, docData, Firestore } from '@angular/fire/firestore';
+import { inject, Injectable } from '@angular/core';
+import {
+  doc,
+  docData,
+  Firestore,
+  increment,
+  updateDoc,
+} from '@angular/fire/firestore';
 import { AppUser } from '../types';
-import { Observable } from 'rxjs';
+import { catchError, from, Observable, of, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  userProfile = {
-    username: 'johndoe',
-    wallet: 1000,
-  };
-  isLogin = signal(false);
-
   fireStore = inject(Firestore);
-
-  login() {
-    this.isLogin.set(true);
-  }
-
-  logout() {
-    this.isLogin.set(false);
-  }
 
   getProfile(uid: string): Observable<AppUser | null> {
     const ref = doc(this.fireStore, 'users', uid);
 
     return docData(ref, { idField: 'id' }) as Observable<AppUser | null>;
+  }
+
+  topup(uid: string, amount: number) {
+    const ref = doc(this.fireStore, 'users', uid);
+
+    return from(updateDoc(ref, {
+      wallet: increment(amount),
+    })).pipe(
+      switchMap(() => of({
+        success: true,
+        message: `เติมเงินสำเร็จ`
+      })),
+      catchError((e) => {
+        console.error(e);
+        return of({
+          success: false,
+          message: 'ไม่สามารถเติมเงินได้กรุณาลองใหม่ภายหลัง',
+        });
+      }),
+    );
   }
 }
